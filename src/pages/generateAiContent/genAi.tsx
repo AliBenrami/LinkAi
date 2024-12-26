82c45d451a8ec2e8aa97b18b9ad6f6f0a0c8d2e2
Ali <111257593+AliBenrami@users.noreply.github.com>
2024-12-26 13:46:24 -0600

    Update genAi.tsx

--- src/pages/generateAiContent/genAi.tsx
+++ src/pages/generateAiContent/genAi.tsx
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("AIzaSyB6_W9ehhIwGKwkFYsLfkgqVRtm_grf3TE");
+const genAI = new GoogleGenerativeAI("");
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export const generateContent = async (prompt: string) => {
